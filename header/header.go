2f9d752853be87d3941e0a628044847d7ca934ed
jimlloyd <jim.lloyd@gmail.com>
2013-09-24 21:39:43 -0700

    Methods for encoding/decoding requests & responses, and for discovering message type
    By 'discovering message type' I mean peeking into a packet with PeekMessageType.
    With this commit it should be possible to accept different message types on
    the same interface and dispatch them correctly.

--- header/header.go
+++ header/header.go
@@ -16,12 +16,14 @@ const (
 	reserved
 )
 
+type MessageType uint16  // One of the constants above
+
 var mbusSignature = [4]byte{'m', 'b', 'u', 's'}
 
 type CommonHeader struct {
 	// all fields are specified as byte arrays, with bytes in network byte order (big endian)
 	Signature	[4]byte	// a constant signature, 'mbus'
-	MsgType		uint16  // One of the constants above
+	MsgType		MessageType 
 }
 
 type MessageHeader struct {
@@ -29,8 +31,37 @@ type MessageHeader struct {
 	Sequence	uint32	// The sequence number of the first byte of the message
 }
 
+type RequestHeader struct {
+	CommonHeader
+}
+
+type ResponseHeader struct {
+	CommonHeader
+}
+
 func MakeMessageHeader(sequence uint32) MessageHeader {
 	return MessageHeader{CommonHeader{mbusSignature, Message}, sequence}
+}
+
+func MakeRequestHeader() RequestHeader {
+	return RequestHeader{CommonHeader{mbusSignature, Request}}
+}
+
+func MakeResponseHeader() ResponseHeader {
+	return ResponseHeader{CommonHeader{mbusSignature, Response}}
+}
+
+func PeekMessageType(packetData []byte) MessageType {
+	var head CommonHeader
+	buf := bytes.NewBuffer(packetData)
+	err := binary.Read(buf, binary.LittleEndian, &head)
+	if err == nil {
+		switch head.MsgType {
+		case Message, Request, Response:
+			return head.MsgType
+		}
+	}
+	return Invalid
 }
 
 func (self *CommonHeader) Valid() bool {
@@ -41,7 +72,15 @@ func (self *MessageHeader) Valid() bool {
 	return self.Signature == mbusSignature && self.MsgType == Message
 }
 
-func (self *CommonHeader) MessageType() (uint16, error) {
+func (self *RequestHeader) Valid() bool {
+	return self.Signature == mbusSignature && self.MsgType == Request
+}
+
+func (self *ResponseHeader) Valid() bool {
+	return self.Signature == mbusSignature && self.MsgType == Response
+}
+
+func (self *CommonHeader) MessageType() (MessageType, error) {
 	if !self.Valid() {
 		return Invalid, InvalidHeaderError{}
 	}
@@ -66,6 +105,26 @@ func (self *MessageHeader) Encode() (*bytes.Buffer, error) {
 	return buf, err
 }
 
+// TODO: Yes, I should declare an interface so that I could write an EncodeImpl
+// that works for all three message types.
+func (self *RequestHeader) Encode() (*bytes.Buffer, error) {
+	buf := new(bytes.Buffer)
+	if !self.Valid() || self.MsgType!=Request {
+		return buf, InvalidHeaderError{}
+	}
+	err := binary.Write(buf, binary.LittleEndian, *self)
+	return buf, err
+}
+
+func (self *ResponseHeader) Encode() (*bytes.Buffer, error) {
+	buf := new(bytes.Buffer)
+	if !self.Valid() || self.MsgType!=Response {
+		return buf, InvalidHeaderError{}
+	}
+	err := binary.Write(buf, binary.LittleEndian, *self)
+	return buf, err
+}
+
 // Decode the header from the bytes slice into this MessageHeader.
 // Returns the Buffer so that application message payload can be retrieved.
 func (self *MessageHeader) Decode(packetData []byte) (*bytes.Buffer, error) {
@@ -73,6 +132,28 @@ func (self *MessageHeader) Decode(packetData []byte) (*bytes.Buffer, error) {
 	err := binary.Read(buf, binary.LittleEndian, self)
 	if err==nil {
 		if !self.Valid() || self.MsgType!=Message {
+			return buf, InvalidHeaderError{}
+		}
+	}
+	return buf, err
+}
+
+func (self *RequestHeader) Decode(packetData []byte) (*bytes.Buffer, error) {
+	buf := bytes.NewBuffer(packetData)
+	err := binary.Read(buf, binary.LittleEndian, self)
+	if err==nil {
+		if !self.Valid() || self.MsgType!=Request {
+			return buf, InvalidHeaderError{}
+		}
+	}
+	return buf, err
+}
+
+func (self *ResponseHeader) Decode(packetData []byte) (*bytes.Buffer, error) {
+	buf := bytes.NewBuffer(packetData)
+	err := binary.Read(buf, binary.LittleEndian, self)
+	if err==nil {
+		if !self.Valid() || self.MsgType!=Response {
 			return buf, InvalidHeaderError{}
 		}
 	}
--- header/header_test.go
+++ header/header_test.go
@@ -6,9 +6,88 @@ import (
 	"testing"
 )
 
+func TestHeaderTypes(t *testing.T) {
+	const seq = uint32(23)
+	message := MakeMessageHeader(seq)
+	request := MakeRequestHeader()
+	response := MakeResponseHeader()
+
+	if !message.Valid() {
+		t.Fail()
+	}
+	if !request.Valid() {
+		t.Fail()
+	}
+	if !response.Valid() {
+		t.Fail()
+	}
+
+	messageBuf, err := message.Encode()
+	if err != nil {
+		t.Fail()
+	}
+	requestBuf, err := request.Encode()
+	if err != nil {
+		t.Fail()
+	}
+	responseBuf, err := response.Encode()
+	if err != nil {
+		t.Fail()
+	}
+
+	var xMessage MessageHeader
+	var xRequest RequestHeader
+	var xResponse ResponseHeader
+
+	_, err = xMessage.Decode(messageBuf.Bytes())
+	if err != nil {
+		t.Fail()
+	}
+	if message != xMessage {
+		t.Fail()
+	}
+
+	_, err = xRequest.Decode(requestBuf.Bytes())
+	if err != nil {
+		t.Fail()
+	}
+	if request != xRequest {
+		t.Fail()
+	}
+
+	_, err = xResponse.Decode(responseBuf.Bytes())
+	if err != nil {
+		t.Fail()
+	}
+	if response != xResponse {
+		t.Fail()
+	}
+
+	if Message != PeekMessageType(messageBuf.Bytes()) {
+		t.Error("PeekMessageType failed to return Message for a Message packet.")
+	}
+	if Request != PeekMessageType(requestBuf.Bytes()) {
+		t.Error("PeekMessageType failed to return Request for a Message packet.")
+	}
+	if Response != PeekMessageType(responseBuf.Bytes()) {
+		t.Error("PeekMessageType failed to return Response for a Message packet.")
+	}
+
+
+	_, err = xResponse.Decode(requestBuf.Bytes())
+	if err == nil {
+		t.Error("A ResponseHeader should not accept decoding from a Request packet")
+	}
+
+	_, err = xMessage.Decode(requestBuf.Bytes())
+	if err == nil {
+		t.Error("A MessageHeader should not accept decoding from a Request packet")
+	}
+}
+
+
 func TestMakeMessageHeader(t *testing.T) {
 	const seq = uint32(23)
-
 	h := MakeMessageHeader(seq)
 
 	if !h.Valid() {
